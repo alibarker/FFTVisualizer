1f494a5de509f1fa8a3a2a0d82322f90d57db3aa
Ali Barker <ali@alibarker.co.uk>
2019-08-08 10:20:57 +0100

    Added visualizer files

---
+++ FFTVisualizer/Source/Visualizer.h
@@ -0,0 +1,250 @@
+/*
+  ==============================================================================
+
+    Visualizer.h
+    Created: 9 Jul 2019 4:29:48pm
+    Author:  Alistair Barker
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "JuceHeader.h"
+
+struct Fifo
+{
+    void addToFifo (const float* someData, int numItems)
+    {
+        int start1, size1, start2, size2;
+        abstractFifo.prepareToWrite (numItems, start1, size1, start2, size2);
+
+        if (size1 > 0)
+            copySomeData (myBuffer + start1, someData, size1);
+
+        if (size2 > 0)
+            copySomeData (myBuffer + start2, someData + size1, size2);
+
+        abstractFifo.finishedWrite (size1 + size2);
+    }
+
+    void readFromFifo (float* someData, int numItems)
+    {
+        int start1, size1, start2, size2;
+        abstractFifo.prepareToRead (numItems, start1, size1, start2, size2);
+
+        if (size1 > 0)
+            copySomeData (someData, myBuffer + start1, size1);
+
+        if (size2 > 0)
+            copySomeData (someData + size1, myBuffer + start2, size2);
+
+        abstractFifo.finishedRead (size1 + size2);
+    }
+
+    void copySomeData (float* dest, const float* source, int numItems)
+    {
+        FloatVectorOperations::copy (dest, source, numItems);
+    }
+
+    AbstractFifo abstractFifo { 4096 };
+    float myBuffer[4096];
+};
+
+class Visualizer : public Component, public Thread
+{
+public:
+    Visualizer (int fftOrder) :
+        Thread ("fft"),
+        fft (fftOrder),
+        windowingFunction (static_cast<size_t> (fft.getSize ()), dsp::WindowingFunction<float>::hamming)
+    {
+        inputBuffer.setSize (1, 16384, false, true);
+        processingBuffer.setSize (1, 2 * fft.getSize (), false, true);
+
+        fftOutputBuffer.setSize (1, fft.getSize () / 2, false, true);
+        fftMaxOutputBuffer.setSize (1, fft.getSize () / 2, false, true);
+
+        startThread ();
+    }
+
+    ~Visualizer ()
+    {
+        stopThread (3000);
+    }
+
+    void setSampleRate (double fs)  {     sampleRate = fs;    }
+    double getSampleRate ()         {     return sampleRate;    }
+
+    int getNumBins () const
+    {
+        return fft.getSize () / 2;
+    }
+
+    void addSamples (const float* samples, int numSamples)
+    {
+        jassert (sampleRate > 0.);
+        fifo.addToFifo (samples, numSamples);
+    }
+
+    void copyCurrentFft (float* samples, int numSamples)
+    {
+        jassert (numSamples == fft.getSize () / 2);
+        ScopedLock lock (processingLock);
+        FloatVectorOperations::copy (samples, fftOutputBuffer.getReadPointer (0), numSamples);
+    }
+
+    bool getMaxHasChanged ()
+    {
+        ScopedLock lock (processingLock);
+        if (! maxHasChanged)
+            return false;
+
+        maxHasChanged = false;
+        return true;
+    }
+
+    void resetMax ()
+    {
+        ScopedLock lock (processingLock);
+        fftMaxOutputBuffer.clear ();
+        maxHasChanged = true;
+    }
+
+    void copyCurrentMax (float* samples, int numSamples)
+    {
+        jassert (numSamples == fft.getSize () / 2);
+        ScopedLock lock (processingLock);
+        FloatVectorOperations::copy (samples, fftMaxOutputBuffer.getReadPointer (0), numSamples);
+    }
+
+private:
+    void run () override
+    {
+        while (! threadShouldExit ())
+        {
+            const auto numReady = fifo.abstractFifo.getNumReady ();
+            if (numReady > 0)
+            {
+                addToInputBuffer (numReady);
+                perform ();
+            }
+
+            sleep (1);
+        }
+    }
+
+    void addToInputBuffer (int numSamples)
+    {
+        const auto bufferSize = inputBuffer.getNumSamples ();
+        if (writePointer + numSamples < bufferSize)
+        {
+            fifo.readFromFifo (inputBuffer.getWritePointer (0, writePointer), numSamples);
+            writePointer += numSamples;
+        }
+        else
+        {
+            const auto numToCopy1 = bufferSize - writePointer;
+            fifo.readFromFifo (inputBuffer.getWritePointer (0, writePointer), numToCopy1);
+
+            const auto numToCopy2 = numSamples - numToCopy1;
+            fifo.readFromFifo (inputBuffer.getWritePointer (0), numToCopy2);
+
+            writePointer = numToCopy2;
+        }
+    }
+
+    void readFromInputBuffer (float* destination, int numSamples)
+    {
+        const auto bufferSize = inputBuffer.getNumSamples ();
+
+        if (readPointer + numSamples < bufferSize)
+        {
+            FloatVectorOperations::copy (destination, inputBuffer.getReadPointer (0, readPointer), numSamples);
+            readPointer += numSamples;
+        }
+        else
+        {
+            const auto numToCopy1 = bufferSize - readPointer;
+            FloatVectorOperations::copy (destination, inputBuffer.getReadPointer (0, readPointer), numToCopy1);
+
+            const auto numToCopy2 = numSamples - numToCopy1;
+            FloatVectorOperations::copy (destination + numToCopy1, inputBuffer.getReadPointer (0), numToCopy2);
+
+            readPointer = numToCopy2;
+        }
+    }
+
+    void copyFromFftBufferToInputBuffer ()
+    {
+        const auto fftSize = fft.getSize ();
+        processingBuffer.clear ();
+        readFromInputBuffer (processingBuffer.getWritePointer (0), fftSize);
+        windowingFunction.multiplyWithWindowingTable (processingBuffer.getWritePointer (0),
+                                                      static_cast<size_t> (fftSize));
+    }
+
+    void perform ()
+    {
+        while (getWrappedDistanceBetweenPointers () > fft.getSize ())
+        {
+            copyFromFftBufferToInputBuffer ();
+            fft.performFrequencyOnlyForwardTransform (processingBuffer.getWritePointer (0));
+            applyBalisticsAndCopyToOutput ();
+        }
+    }
+
+    void applyBalisticsAndCopyToOutput ()
+    {
+        ScopedLock sl (processingLock);
+        auto input = processingBuffer.getWritePointer (0);
+        auto output = fftOutputBuffer.getWritePointer (0);
+        auto maxOutput = fftMaxOutputBuffer.getWritePointer (0);
+
+        const auto decayRate = Decibels::decibelsToGain (-40.f * fft.getSize () / sampleRate);
+
+        for (int n = 0 ; n < fftOutputBuffer.getNumSamples (); ++n)
+        {
+            if (input[n] > output[n])
+            {
+                output[n] = input[n];
+            }
+            else
+                output[n] *= decayRate;
+
+
+            if (input[n] > maxOutput[n])
+            {
+                maxOutput[n] = input[n];
+                maxHasChanged = true;
+            }
+        }
+    }
+
+    int getWrappedDistanceBetweenPointers ()
+    {
+        if (writePointer > readPointer)
+            return writePointer - readPointer;
+
+        return inputBuffer.getNumSamples () - readPointer + writePointer;
+    }
+
+    double sampleRate {0.};
+
+    Fifo fifo;
+    AudioBuffer<float> processingBuffer;
+    AudioBuffer<float> fftOutputBuffer;
+    AudioBuffer<float> fftMaxOutputBuffer;
+    AudioBuffer<float> inputBuffer;
+
+    dsp::FFT fft;
+
+    dsp::WindowingFunction<float> windowingFunction;
+
+    bool maxHasChanged {false};
+
+    int writePointer {0};
+    int readPointer {0};
+
+    CriticalSection processingLock;
+};
---
+++ FFTVisualizer/Source/VisualizerComponent.h
@@ -0,0 +1,199 @@
+/*
+  ==============================================================================
+
+    VisualizerComponent.h
+    Created: 10 Jul 2019 1:31:21pm
+    Author:  Alistair Barker
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "JuceHeader.h"
+#include "Visualizer.h"
+
+class VisualizerComponent : public Component
+{
+public:
+    VisualizerComponent (Visualizer& visualizer) : Component ("FFTDisplay"), visualizer (visualizer)
+    {
+        fftInputBuffer.setSize (1, visualizer.getNumBins (), false, true);
+        maxInputBuffer.setSize (1, visualizer.getNumBins (), false, true);
+
+        redrawTimer.setCallbackFunction ([this] () { update (); });
+        redrawTimer.startTimerHz (60);
+
+        maxResetTimer.setCallbackFunction ([this] () { resetMax (); });
+
+        addAndMakeVisible (fftGraph);
+        addAndMakeVisible (maxGraph);
+    }
+
+    void resized () override
+    {
+        maxGraph.setBounds (getLocalBounds ());
+        fftGraph.setBounds (getLocalBounds ());
+    }
+
+    void resetMax ()
+    {
+        visualizer.resetMax ();
+        maxResetTimer.stopTimer ();
+    }
+
+private:
+    Visualizer& visualizer;
+    AudioBuffer<float> fftInputBuffer;
+    AudioBuffer<float> maxInputBuffer;
+
+    class MaxGraph : public Component
+    {
+    public:
+        MaxGraph ()
+        {
+            setBufferedToImage (true);
+        }
+
+        void paint (Graphics& g) override
+        {
+            const auto numSegments = 50;
+            const auto numPixelsPerSegment = static_cast<int> (std::ceil (static_cast<float> (getWidth ()) / numSegments));
+
+            for (int i = 0; i < numSegments; ++i)
+            {
+                const auto start = i * numPixelsPerSegment;
+                const auto end = jmin (start + numPixelsPerSegment + 1, getWidth ());
+
+                drawLineSegment (g, start, end);
+            }
+        }
+
+        void drawLineSegment (Graphics& g, int start, int end)
+        {
+            Path path;
+
+            auto buffer = renderBuffer.getReadPointer (0);
+
+            const auto height = getHeight ();
+
+            auto prevX = static_cast<float> (start);
+            auto prevY = height - buffer[start] * height;
+
+            path.startNewSubPath ({prevX, prevY});
+
+            for (int i = start + 1; i < end; ++i)
+            {
+                const auto x = static_cast<float> (i);
+                const auto y = height - buffer [i] * height;
+
+                path.addLineSegment ({{prevX, prevY}, {x, y}}, 1.f);
+                prevX = x;
+                prevY = y;
+            }
+
+            g.setColour (Colours::whitesmoke);
+            g.fillPath (path);
+        }
+
+        void resized () override
+        {
+            renderBuffer.setSize (1, getWidth ());
+        }
+
+        AudioBuffer<float> renderBuffer;
+    };
+
+    class FftGraph : public Component
+    {
+    public:
+        FftGraph ()
+        {
+            setOpaque (true);
+            setPaintingIsUnclipped (true);
+        }
+
+        void paint (Graphics& g) override
+        {
+            g.setColour (Colours::black);
+            g.fillRect (getLocalBounds ());
+            g.setColour (Colours::whitesmoke.withAlpha (0.2f));
+            const auto height = static_cast<float> (getHeight ());
+
+            auto buffer = renderBuffer.getReadPointer (0);
+
+            for (int i = 0; i < getWidth (); ++i)
+            {
+                const auto top = getHeight () - buffer[i] * height;
+                g.drawVerticalLine (i, top, height);
+            }
+        }
+
+        void resized () override
+        {
+            renderBuffer.setSize (1, getWidth ());
+        }
+
+        AudioBuffer<float> renderBuffer;
+    };
+
+    FftGraph fftGraph;
+    MaxGraph maxGraph;
+
+    MiscUtils::LambdaTimer redrawTimer;
+    MiscUtils::LambdaTimer maxResetTimer;
+
+    void update ()
+    {
+        if (isVisible ())
+        {
+            visualizer.copyCurrentFft (fftInputBuffer.getWritePointer (0), visualizer.getNumBins ());
+            updateRenderBuffer (fftGraph.renderBuffer, fftInputBuffer, getWidth (), visualizer.getNumBins ());
+            fftGraph.repaint ();
+
+            if (visualizer.getMaxHasChanged ())
+            {
+                visualizer.copyCurrentMax (maxInputBuffer.getWritePointer (0), visualizer.getNumBins ());
+                updateRenderBuffer (maxGraph.renderBuffer, maxInputBuffer, getWidth (), visualizer.getNumBins ());
+                maxGraph.repaint ();
+                maxResetTimer.startTimer (5000);
+            }
+        }
+    }
+
+    static void updateRenderBuffer (AudioBuffer<float>& dest, const AudioBuffer<float>& source, int width, int numBins)
+    {
+        const auto fft = source.getReadPointer (0);
+        auto destination = dest.getWritePointer (0);
+
+        auto previousValue = getRelativeDbValue (fft[0], numBins);
+
+        for (int i = 0; i < width; ++i)
+        {
+            const auto normPos = static_cast<float> (i) / width;
+
+            const auto binPos = RangeUtils::normalizedToLogRange (normPos, 1.f, numBins);
+            const auto bin = static_cast<int> (std::floor (binPos));
+            const auto nextBin = bin + 1 < numBins ? bin + 1 : bin;
+            const auto posInBin = binPos - bin;
+
+            const auto lower = getRelativeDbValue (fft[bin], numBins);
+            const auto upper = getRelativeDbValue (fft[nextBin], numBins);
+
+            const auto interpolatedValue = lower - posInBin * (lower - upper);
+            const auto smoothedValue = 0.5f * (previousValue + interpolatedValue);
+
+            previousValue = smoothedValue;
+
+            destination [i] = smoothedValue;
+        }
+    }
+
+    static float getRelativeDbValue (float fftValue, int numBins)
+    {
+        const auto scaledValue = fftValue / (2 * numBins);
+        const auto dBValue = Decibels::gainToDecibels (scaledValue, -100.f);
+
+        return dBValue / 100.f + 1.f;
+    }
+};
